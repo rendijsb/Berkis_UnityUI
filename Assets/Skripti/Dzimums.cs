7cebe97b03bf36dce1cd67a9059fe3a0ee89361b
Rendijs <rendijsberkis7@gmail.com>
2023-04-28 17:55:49 +0300

    Sievietes ainas sākums

---
+++ Assets/Skripti/Dzimums.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Dzimums : MonoBehaviour {
+	//Metode ļauj pārslēgties no UI ainas uz Sākuma ainu
+	public void sieviete(){
+	SceneManager.LoadScene("Sieviete", LoadSceneMode.Single);
+	}
+	public void virietis(){
+	SceneManager.LoadScene("Virietis", LoadSceneMode.Single);
+	}
+
+
+	//Metode, kura aizver programmu, ja tā ir izveidota kā .exe
+	public void Apturet()
+	{
+		Application.Quit();
+	}
+
+	//Pašiem jāuztaisa metode, kas pārslēdz no sākuma uz UI ainu
+
+	
+}
