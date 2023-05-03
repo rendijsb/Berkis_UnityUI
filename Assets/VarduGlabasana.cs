81c4284b47a399ae21ccf13f4f8d3fa6f1577f8e
unknown <alekss.mozajevs@sk.lvt.lv>
2023-05-03 11:13:59 +0300

    Jaunas ainas, skriptu izveide, telu izmeru mainīšana

---
+++ Assets/VarduGlabasana.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+
+using System.Collections.Generic;
+
+using UnityEngine;
+
+using UnityEngine.UI;
+
+public class VarduGlabasana : MonoBehaviour {
+
+	public string teksts;
+
+	public string teksts1;
+
+	public GameObject ievadesLauks;
+
+	public GameObject tekstaAttelosana;
+
+	public GameObject ievadesLauks1;
+
+	public GameObject tekstaAttelosana1;
+
+	public void UzglabatTekstu(){
+
+		teksts = ievadesLauks.GetComponent<InputField>().text;
+
+		tekstaAttelosana.GetComponent<Text>().text = teksts;
+
+		teksts1 = ievadesLauks1.GetComponent<InputField>().text;
+
+		tekstaAttelosana1.GetComponent<Text>().text = teksts1;
+
+		teksts1 = ievadesLauks1.GetComponent<InputField>().text;
+
+		tekstaAttelosana1.GetComponent<Text>().text = teksts1;
+
+	}
+
+}
---
+++ Assets/drebjuParvietosana.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+
+using System.Collections.Generic;
+
+using UnityEngine;
+
+//Importē, lai lietotu pointer darbību interfeisu
+
+using UnityEngine.EventSystems;
+
+//Piesaista interfeisu
+
+public class drebjuParvietosana : MonoBehaviour,IPointerDownHandler, IBeginDragHandler, IDragHandler,IEndDragHandler {
+
+	private RectTransform transformKomponente;
+
+	public Canvas kanva;
+
+	// Use this for initialization
+
+	void Start () {
+
+		//startējot automātiski piekļūst attēla transform komponentei un piefiksē kādas koordinātās attēls atrodas
+
+		transformKomponente = GetComponent<RectTransform>();
+
+	}
+
+	public void OnPointerDown(PointerEventData notikums){
+
+		Debug.Log ("Izdarīts peles klikšķis uz objekta!");
+
+	}
+
+	public void OnBeginDrag(PointerEventData notikums){
+
+		Debug.Log ("Uzsākta objekta vilkšana!");
+
+	}
+
+	public void OnDrag(PointerEventData notikums){
+
+		Debug.Log ("Notiek objekta pāŗvietošana!");
+
+		transformKomponente.anchoredPosition += notikums.delta / kanva.scaleFactor;
+
+	}
+
+	public void OnEndDrag(PointerEventData notikums){
+
+		Debug.Log ("Pabeigta objekta vilkšana!");
+
+	}
+
+}
